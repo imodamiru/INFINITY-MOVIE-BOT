34e59878c5f066825df712b8f061cd7659d5b613
imodamiru <damiruimo@gmail.com>
2024-11-23 12:16:23 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,10 +5,10 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "", //your session id
-MV_SEND_JID: process.env.MV_SEND_JID || "", //default movie send group
+SESSION_ID: process.env.SESSION_ID || "A-S-W-I-N-S-P-A-R-K-Y:55b179b399c33fafafef5a556e087469", //your session id
+MV_SEND_JID: process.env.MV_SEND_JID || "94743747913@s.whatsapp.net", //default movie send group
 MODE: process.env.MODE || "public", //private or inbox or groups
 AUTO_AI: process.env.AUTO_AI || "on", //on or off
-AUTO_AI_JID: process.env.AUTO_AI_JID || "", //auto ai working group
+AUTO_AI_JID: process.env.AUTO_AI_JID || "94743747913@s.whatsapp.net", //auto ai working group
 BLOCK_JID: process.env.BLOCK_JID || "", //block jids
 };
